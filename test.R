2066e84af6e6def42106f5024a734c6629f2a245
Joe Barnett <64959208+jbarnettapr@users.noreply.github.com>
2024-01-25 15:04:22 +0000

    Try some automated testing

--- test.R
+++ test.R
@@ -1,1 +1,4 @@
-"test"
+print("Hello world")
+
+plot(x = 1:10, y = (1:10)^2)
+lines(x = 1:10, y = (10:1)^2)
---
+++ test/unitTests.R
@@ -0,0 +1,5 @@
+library(testthat)
+
+testthat::test_that("Make sure some very stable stuff hasn't been broken", {
+    expect_equal(1, 1)
+})
